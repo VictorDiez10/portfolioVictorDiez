e48b8d04d7688253e413be36c62357ef7e91a0de
Victor Diez <victordiez1@gmail.com>
2024-10-10 15:39:08 +0200

    Portfolio

---
+++ data/index.ts
@@ -0,0 +1,118 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Approach", link: "#approach" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Je privilégie la collaboration avec les clients, favoriser une communication ouverte ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Où que vous soyez",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Les technologies utilisé",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Passionné de technologie et de développement.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "J'essaie constamment de m'améliorer",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Vous souhaitez démarrer un projet ensemble ?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Booki - Agence de voyage",
+      des: "Page d'accueil d'une agence de voyage avec HTML & CSS. Des hotels, des chambres d'hôtes en passant par les activités",
+      img: "/a-1.png",
+      iconLists: ["/html.svg", "/css.svg"],
+      link: "https://github.com/VictorDiez10/Projet-2-html-css",
+    },
+    {
+      id: 2,
+      title: "Kasa - Page web de location avec React",
+      des: "Cherchez votre prochain hébergement. Où vous voulez dans toutes la France.",
+      img: "/a-2.png",
+      iconLists: ["/re.svg", "/sass.png", "/rero.svg"],
+      link: "https://github.com/VictorDiez10/projet_6_Kasa",
+    },
+    {
+      id: 3, 
+      title: "Mon vieux Grimoire - Back-end",
+      des: "Créez un compte pour ajouter vos livres favoris et noter vos livres préférés.",
+      img: "/a-3.png",
+      iconLists: ["/mdb.svg", "/node.svg", "/express.svg"],
+      link: "https://github.com/VictorDiez10/backend",
+    },
+    {
+      id: 4,
+      title: "Sophie Bluel - Portfolio designer avec JavaScript",
+      des: "Aller voir le Portfolio de Sophie Bluel Designer d'intérieur. Créer un compte aussi pour ajouter des photos !",
+      img: "/a-4.png",
+      iconLists: ["/node.svg", "/js.svg", "/swagger.svg", "/html.svg", "/css.svg"],
+      link: "https://github.com/VictorDiez10/Projet_3_javascript",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href: "https://github.com/VictorDiez10",
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+      href: "https://www.linkedin.com/in/victor-10-diez/",
+    },
+  ];
